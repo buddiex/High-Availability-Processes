1ff7733af86903f49d3685709618ad128aefa67a
Olaniyi Falomo <niyifalomo@gmail.com>
2018-12-08 04:25:40 -0500

    Start backup service with args when primary starts

--- ha/server/tuple_space_server.py
+++ ha/server/tuple_space_server.py
@@ -1,77 +1,88 @@
-import re
 import subprocess
 import sys
-from ha.commons.server import HeartbeatServer, HearbeatRequestHandler
 import json
 import argparse
+import os
+from ha.commons.logger import get_module_logger
 
-
-class ServiceCommandLineArguments(object):
-    def __int__(self, tuple_space_file=None):
-        self.tuple_space_file = tuple_space_file
-        self.shutdown_sap = ("localhost",8999)
-        self.heartbeat_sap = ("localhost",8999)
-        self.isPrimary = False
+logger = get_module_logger(__name__)
 
 
 class TupleSpaceService:
 
-    def __init__(self, command_args:ServiceCommandLineArguments):
-        self.tuple_space_file = command_args.tuple_space_file
-        self.tuple_space = ""
-        self.tuple_space_regex = ""
-        self.shutdown_sap = command_args.shutdown_sap
-        self.hearbeat_sap = command_args.heartbeat_sap
-        self.isPrimary = command_args.isPrimary
+    def __init__(self, parsed_args: argparse.ArgumentParser()):
+        self.tuple_space_file = parsed_args.tuple_space_file
+        self.tuple_space = None
+        self.shutdown_sap = parsed_args.shutdown_sap
+        self.proxy_sap = parsed_args.proxy_sap
+        self.backup_sap = parsed_args.backup_sap
+        self.primary_sap = parsed_args.primary_sap
+        self.isPrimary = parsed_args.is_primary_server
         self.server_script_name = ""
+        self.primary_process_id = ""
+
+        # Get command line arguments to be used by backup_service
+        self.raw_command_args = sys.argv[1:]
 
     def initialize(self):
-        if self.isPrimary:
+        """ Initial Tuple Space service"""
+
+        if self.isPrimary is True:
             self.start_as_primary()
         else:
             self.start_as_backup()
 
     def load_tuple_space(self) -> None:
+        """ Load tuple space from tuple space file"""
         try:
             if self.tuple_space_file is not None:
                 with open(self.tuple_space_file, "r") as file:
                     file_content = file.read().strip()
-
                     try:
                         self.tuple_space = json.loads(file_content)
                     except ValueError as err:
-                       raise
+                        raise
 
         except Exception as err:
             raise
 
     def start_backup(self) -> None:
+        """ Start backup service with specific arguments"""
+        # use os.getpid() get process id that can be used to kill the primary process
+        # add additional parameters for the backup service
+        # -b flags it as a backup service
 
-        subprocess.Popen(["python", self.server_script_name], shell=False)
+        backup_args = self.raw_command_args + ['-primary_id', str(os.getpid()), '-b', 'yes']
+
+        # if the backup can't be started, write the tuple space to file
+        try:
+            logger.info("Starting backup service")
+
+            subprocess.Popen(["python", self.server_script_name] + backup_args, shell=False)
+        except:
+            raise
+            if self.tuple_space is not None:
+                with open(self.tuple_space_file, "w") as file:
+                    file.write(self.tuple_space)
 
     def start_heartbeat_socket(self):
-        #server = HeartbeatServer(HearbeatRequestHandler, host, port)
-        #server.serve_forever()
         pass
 
     def start_shutdown_socket(self):
-        # use ServeSideClientConn
-
         pass
 
     def start_update_socket(self):
-        # use ServeSideClientConn
         pass
 
     def start(self):
         pass
 
     def start_as_primary(self):
-
+        logger.info("Starting primary service")
         self.load_tuple_space()
         self.start_backup()
         self.start_heartbeat_socket()
-        #self.wait_for_backup()
+        # self.wait_for_backup()
 
         pass
 
@@ -80,30 +91,40 @@ class TupleSpaceService:
 
 
 if __name__ == "__main__":
-    # get current script name
+    # get current script name - used to start backup service
     script_name = sys.argv[0]
 
-    # Get command line arguments
-    command_line_args = ServiceCommandLineArguments()
+    # Parse command line arguments
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-primary', '--primary-server',
-                        dest='is_primary_server', default=False)
+
+    parser.add_argument('-tpfile', '--tuple-space-file',
+                        dest='tuple_space_file', default="sample_tuple_file.json")
+    parser.add_argument('-proxy', '--proxy-sap',
+                        dest='proxy_sap', default=('localhost', 8999))
+    parser.add_argument('-backup', '--backup-sap',
+                        dest='backup_sap', default=('localhost', 8999))
+    parser.add_argument('-primary', '--primary-sap',
+                        dest='primary_sap', default=('localhost', 8999))
+    parser.add_argument('-p', '--is-primary-server',
+                        dest='is_primary_server', default=None)
+    parser.add_argument('-shutdown', '--shutdown-sap',
+                        dest='shutdown_sap', default=('localhost', 8999))
+    parser.add_argument('-primary_id', '--primary-process-id',
+                        dest='primary_id', default=None)
+    parser.add_argument('-b', '--is-backup-server',
+                        dest='is_backup_server', default=None)
 
     parsed_args = parser.parse_args()
-    command_line_args.isPrimary = False if parsed_args.is_primary_server is False else True
-    command_line_args.tuple_space_file = "sample_tuple_file.json"
-    command_line_args.shutdown_sap =("localhost",8999)
-    command_line_args.heartbeat_sap = ("localhost", 8999)
-
-    # cl arguments end
+    if parsed_args.is_backup_server is not None and parsed_args.is_backup_server is not False:
+        parsed_args.is_primary_server = False
+    else:
+        parsed_args.is_primary_server = True
 
     # initialize tuple space service
-    tuple_space_service = TupleSpaceService(command_line_args)
+    tuple_space_service = TupleSpaceService(parsed_args)
     tuple_space_service.server_script_name = script_name
     tuple_space_service.initialize()
-    tuple_space_service.start_as_primary()
 
-    print(tuple_space_service.tuple_space)
+    # print(tuple_space_service.tuple_space)
 
-    # start backup service

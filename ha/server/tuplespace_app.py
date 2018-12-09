e9867e1476c6ba5b4943f24962d579dad875a1bc
osayamen <mailbuddie@gmail.com>
2018-12-09 13:08:17 -0500

    add tuple space app

---
+++ ha/server/tuplespace_app.py
@@ -0,0 +1,61 @@
+import json
+
+
+class TupleSpaceApp:
+
+    def __init__(self, tuple_space_file):
+
+        self.tuple_space_file = tuple_space_file
+        self.tuple_space = {}
+        pass
+
+    def get(self, data):
+        print(data)
+
+        try:
+            tp = eval(data)
+        except:
+            return self.error_msg('invlaid')
+
+        return self.ok_msg('this is ur result')
+
+    def post(self, data):
+        return "do post"
+
+    def put(self, data):
+        # self.tuple_space.update()
+        try:
+            tp = eval(data)
+            if not isinstance(tp, list):
+                raise ValueError("not list")
+        except:
+            return self.error_msg('invlaid')
+
+        for t in tp:
+            self.tuple_space[t[0]] = t[1]
+
+        return self.ok_msg(f'{len(tp)} tuples saved')
+
+    def delete(self, data):
+
+        return "do delete"
+
+    def ok_msg(self, msg):
+        return 'ok:'+msg
+
+    def error_msg(self, msg):
+        return 'error:'+msg
+
+    def load_tuple_space(self) -> None:
+        """ Load tuple space from tuple space file"""
+        try:
+            if self.tuple_space_file is not None:
+                with open(self.tuple_space_file, "r") as file:
+                    file_content = file.read().strip()
+                    try:
+                        self.tuple_space = json.loads(file_content)
+                    except ValueError as err:
+                        raise
+
+        except Exception as err:
+            raise
